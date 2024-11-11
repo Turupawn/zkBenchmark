70f4cbc85076c5117edfc5aa4d3c723b3ab85157
Turupawn <ahmed.hn.43@gmail.com>
2024-11-11 14:09:54 +0700

    fix

--- generate_proof_noir.sh
+++ generate_proof_noir.sh
@@ -2,10 +2,13 @@
 set -e
 
 # Install Noir if not already installed
+echo "Installing Noir..."
 if ! command -v noir &> /dev/null; then
-    curl -L noirup.dev | bash
-    source ~/.bashrc
-    noirup
+  mkdir -p $HOME/.nargo/bin && \
+  curl -o $HOME/.nargo/bin/nargo-x86_64-unknown-linux-gnu.tar.gz -L https://github.com/noir-lang/noir/releases/download/v0.22.0/nargo-x86_64-unknown-linux-gnu.tar.gz && \
+  tar -xvf $HOME/.nargo/bin/nargo-x86_64-unknown-linux-gnu.tar.gz -C $HOME/.nargo/bin/ && \
+  echo -e '\nexport PATH=$PATH:$HOME/.nargo/bin' >> ~/.bashrc && \
+  source ~/.bashrc
 fi
 
 # Proceed with the rest of the script for proof generation
