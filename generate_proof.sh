4253dc6292d00b02bacb8483641e962e60ae19f1
Turupawn <ahmed.hn.43@gmail.com>
2024-11-11 13:05:05 +0700

    switched to milliseconds

--- generate_proof.sh
+++ generate_proof.sh
@@ -11,37 +11,42 @@ fi
 
 # Compile the circuit
 echo "Compiling circuit..."
-start_compile=$(date +%s)
+start_compile=$(date +%s%3N)  # Time in milliseconds
 circom example.circom --r1cs --wasm --sym
-end_compile=$(date +%s)
+end_compile=$(date +%s%3N)
 compile_time=$((end_compile - start_compile))
-echo "Circuit compilation took $compile_time seconds."
+echo "Circuit compilation took $compile_time milliseconds."
 
 # Generate witness
 echo "Generating witness..."
-start_witness=$(date +%s)
+start_witness=$(date +%s%3N)
 node example_js/generate_witness.js example_js/example.wasm input.json witness.wtns
-end_witness=$(date +%s)
+end_witness=$(date +%s%3N)
 witness_time=$((end_witness - start_witness))
-echo "Witness generation took $witness_time seconds."
+echo "Witness generation took $witness_time milliseconds."
+
+# Generate proving and verification keys, and generate proof
+echo "Setting up and generating proof..."
+start_proof=$(date +%s%3N)
+snarkjs groth16 setup example.r1cs ppot_0080_10.ptau example.zkey
+
+# Generate the verification key
+snarkjs zkey export verificationkey example.zkey verification_key.json
 
 # Generate proof
-echo "Setting up and generating proof..."
-start_proof=$(date +%s)
-snarkjs groth16 setup example.r1cs ppot_0080_10.ptau example.zkey
 snarkjs groth16 prove example.zkey witness.wtns proof.json public.json
-end_proof=$(date +%s)
+end_proof=$(date +%s%3N)
 proof_time=$((end_proof - start_proof))
-echo "Proof generation took $proof_time seconds."
+echo "Proof generation took $proof_time milliseconds."
 
 # Verify proof
 #echo "Verifying proof..."
-#start_verify=$(date +%s)
+#start_verify=$(date +%s%3N)
 #snarkjs groth16 verify verification_key.json public.json proof.json
-#end_verify=$(date +%s)
+#end_verify=$(date +%s%3N)
 #verify_time=$((end_verify - start_verify))
-#echo "Proof verification took $verify_time seconds."
+#echo "Proof verification took $verify_time milliseconds."
 
 # Summary of times
 total_time=$((compile_time + witness_time + proof_time + verify_time))
-echo "Total time taken: $total_time seconds."
+echo "Total time taken: $total_time milliseconds."
