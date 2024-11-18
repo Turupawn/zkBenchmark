7f3f2bf490e0006a8a4738676162f5f333b1e4ba
Turupawn <ahmed.hn.43@gmail.com>
2024-11-18 20:00:59 +0700

    test

--- generate_proof_noir.sh
+++ generate_proof_noir.sh
@@ -5,53 +5,43 @@ set -e
 echo "Installing Noir..."
 if ! command -v noir &> /dev/null; then
   mkdir -p $HOME/.nargo/bin && \
-  curl -o $HOME/.nargo/bin/nargo-x86_64-unknown-linux-gnu.tar.gz -L https://github.com/noir-lang/noir/releases/download/v0.22.0/nargo-x86_64-unknown-linux-gnu.tar.gz && \
+  curl -o $HOME/.nargo/bin/nargo-x86_64-unknown-linux-gnu.tar.gz -L https://github.com/noir-lang/noir/releases/download/v0.36.0/nargo-x86_64-unknown-linux-gnu.tar.gz && \
   tar -xvf $HOME/.nargo/bin/nargo-x86_64-unknown-linux-gnu.tar.gz -C $HOME/.nargo/bin/ && \
   echo -e '\nexport PATH=$PATH:$HOME/.nargo/bin' >> ~/.bashrc && \
   source ~/.bashrc
+  curl -L bbup.dev | bash
+  bbup
 fi
 
 # Proceed with the rest of the script for proof generation
 echo "Proceeding with proof generation..."
 
-# Compile the Fibonacci Noir circuit
-echo "Compiling Fibonacci Noir circuit..."
-start_compile=$(date +%s%3N)  # Time in milliseconds
-noir compile fibonacci.noir --output fibonacci
-end_compile=$(date +%s%3N)
-compile_time=$((end_compile - start_compile))
-echo "Circuit compilation took $compile_time milliseconds."
+cd fibonacci_noir
 
-# Generate witness
-echo "Generating witness for Fibonacci..."
-start_witness=$(date +%s%3N)
-noir generate-witness --circuit fibonacci.r1cs --input inputNoir.json --witness witness.wtns
-end_witness=$(date +%s%3N)
-witness_time=$((end_witness - start_witness))
-echo "Witness generation took $witness_time milliseconds."
+# Check the Fibonacci Noir circuit
+echo "Checking Fibonacci Noir circuit..."
+start_check=$(date +%s%3N)  # Time in milliseconds
+nargo check
+end_check=$(date +%s%3N)
+check_time=$((end_check - start_check))
+echo "Circuit check took $check_time milliseconds."
+
+# Execute
+echo "Executing Fibonacci..."
+start_execute=$(date +%s%3N)
+nargo execute
+end_execute=$(date +%s%3N)
+execute_time=$((end_execute - start_execute))
+echo "Execution took $execute_time milliseconds."
 
 # Generate proving and verification keys, and generate proof
 echo "Setting up and generating proof..."
 start_proof=$(date +%s%3N)
-noir setup fibonacci.r1cs fibonacci.ptau fibonacci.zkey
-
-# Generate the verification key
-noir export-verification-key fibonacci.zkey verification_key.json
-
-# Generate proof
-noir generate-proof --zkey fibonacci.zkey --witness witness.wtns --proof proof.json --public-output public.json
+~/.bb/bb prove -b ./target/fibonacci.json -w ./target/fibonacci.gz -o ./target/proo
 end_proof=$(date +%s%3N)
 proof_time=$((end_proof - start_proof))
 echo "Proof generation took $proof_time milliseconds."
 
-# Verify proof
-#echo "Verifying proof..."
-#start_verify=$(date +%s%3N)
-#noir verify-proof --verification-key verification_key.json --proof proof.json --public-input public.json
-#end_verify=$(date +%s%3N)
-#verify_time=$((end_verify - start_verify))
-#echo "Proof verification took $verify_time milliseconds."
-
 # Summary of times
-total_time=$((compile_time + witness_time + proof_time + verify_time))
+total_time=$((check_time + execute_time + proof_time))
 echo "Total time taken: $total_time milliseconds."
