9b0482517732331976a49c18d38388791ef34795
Turupawn <ahmed.hn.43@gmail.com>
2024-11-20 17:42:00 +0700

    added frontend table

--- frontend/app.js
+++ frontend/app.js
@@ -2,18 +2,31 @@ import { BarretenbergBackend } from '@noir-lang/backend_barretenberg';
 import { Noir } from '@noir-lang/noir_js';
 import circuit from './noirCircuit.json';
 
-const sendProof = async (x, y) => {
-    const backend = new BarretenbergBackend(circuit);
-    const noir = new Noir(circuit, backend);
-    const input = { n: x };
-    document.getElementById("web3_message").textContent="Generating proof... ⌛"
-    var proof = await noir.generateFinalProof(input);
-    document.getElementById("web3_message").textContent="Generating proof... ✅"
-    proof = "0x" + ethereumjs.Buffer.Buffer.from(proof.proof).toString('hex')
-    y = ethereumjs.Buffer.Buffer.from([y]).toString('hex')
-    y = "0x" + "0".repeat(64-y.length) + y
-
-    document.getElementById("public_input").textContent = "public input: " + y
-    document.getElementById("proof").textContent = "proof: " + proof
+const sendProof = async () => {
+  const backend = new BarretenbergBackend(circuit);
+  const noir = new Noir(circuit, backend);
+  const input = { n: 42 };
+  document.getElementById("app_message").textContent="Generating proof... ⌛"
+  const startTime = performance.now();
+  var proof = await noir.generateFinalProof(input);
+  const endTime = performance.now();
+  document.getElementById("app_message").textContent="Generating proof... ✅"
+  proof = "0x" + ethereumjs.Buffer.Buffer.from(proof.proof).toString('hex')
+  document.getElementById("proof").textContent = "zkSNARK: " + proof
+  const elapsedTime = ((endTime - startTime) / 1000).toFixed(4);
+  alert(`It took ${elapsedTime} seconds to generate a proof on your device.`);
 }
+
+const sendProofCircom = async () => {
+  document.getElementById("app_message").textContent="Generating proof... ⌛"
+  const startTime = performance.now();
+  const { proof, publicSignals } = await snarkjs.groth16.fullProve( { n: 42, fibo_of_n: 267914296}, "./circomCircuit.wasm", "./circomCircuit.zkey");
+  const endTime = performance.now();
+  document.getElementById("app_message").textContent="Generating proof... ✅"
+  document.getElementById("proof").textContent = "zkSNARK: " + JSON.stringify(proof)
+  const elapsedTime = ((endTime - startTime) / 1000).toFixed(4);
+  alert(`It took ${elapsedTime} seconds to generate a proof on your device.`);
+}
+
 window.sendProof=sendProof;
+window.sendProofCircom=sendProofCircom;
