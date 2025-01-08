65ed066a8914e4abd1ec509b7ff760bdf67be2f2
Turupawn <ahmed.hn.43@gmail.com>
2025-01-08 19:13:11 +0900

    added openvm

---
+++ fibonacci_openvm/src/main.rs
@@ -0,0 +1,17 @@
+// src/main.rs
+use openvm::io::{read, reveal};
+
+openvm::entry!(main);
+
+fn main() {
+    let n: u64 = read();
+    let mut a: u64 = 0;
+    let mut b: u64 = 1;
+    for _ in 0..n {
+        let c: u64 = a.wrapping_add(b);
+        a = b;
+        b = c;
+    }
+    reveal(a as u32, 0);
+    reveal((a >> 32) as u32, 1);
+}
---
+++ generate_proof_openvm.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e
+
+echo "Installing OpenVM..."
+rustup toolchain install nightly
+cargo +nightly install --git http://github.com/openvm-org/openvm.git cargo-openvm
+rustup component add rust-src --toolchain nightly-2024-10-30-x86_64-unknown-linux-gnu
+cargo openvm --version
+
+# Proceed with the rest of the script for proof generation
+echo "Proceeding with proof generation..."
+
+cd fibonacci_openvm
+
+# Build the project
+cargo openvm build
+
+# Prove the Fibonacci circuit
+echo "Checking Fibonacci OpenVM circuit..."
+start_check=$(date +%s%3N)  # Time in milliseconds
+#OPENVM_FAST_TEST=1 cargo openvm prove app --input "0x0A00000000000000"
+cargo openvm prove app --input "0x0A00000000000000"
+end_proof=$(date +%s%3N)
+proof_time=$((end_proof - start_proof))
+echo "Circuit check took $proof_time milliseconds."
+
+# Summary of times
+total_time=$((proof_time))
+echo "Total time taken: $total_time milliseconds."
